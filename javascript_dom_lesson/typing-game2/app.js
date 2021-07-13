a709d9bcdb047481713412fcb18d6676b2bc53f6
misawa-tasuku <plusnine339@gmail.com>
2021-07-14 03:44:45 +0900

    タイピングゲーム作成

---
+++ javascript_dom_lesson/typing-game2/app.js
@@ -0,0 +1,41 @@
+var p = document.getElementById('text');
+
+var textLists = [
+  'Hello World',
+    'This is my App',
+    'How are you?',
+    'Hello Hello',
+    'I love JavaScript!',
+    'Good morning',
+    'I am Japanese',
+    'Let it be'
+];
+
+var checkTexts = [];
+
+createText();
+
+function createText(){
+  var rnd = Math.floor(Math.random() * textLists.length);
+
+  p.textContent = '';
+
+  checkTexts = textLists[rnd].split('').map(function(value) {
+    var span = document.createElement('span');
+    span.textContent = value;
+    p.appendChild(span);
+    return span;
+  });
+}
+
+document.addEventListener('keydown', keyDown);
+
+function keyDown(e){
+  if(e.key === checkTexts[0].textContent) {
+    checkTexts[0].className = 'add-blue';
+    
+    checkTexts.shift();
+
+    if(!checkTexts.length) createText();
+  }
+}
