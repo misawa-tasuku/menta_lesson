d9ba93762752745c626233ef00963042d5061117
misawa-tasuku <plusnine339@gmail.com>
2021-08-12 13:55:35 +0900

    separate-components

---
+++ React/event-log-app/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, {useReducer} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import reducer from '../reducers'
+import Events from './Events'
+import EventForm from './EventForm'
+
+export const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  
+  return (
+    <div className="container-fluid">
+      <EventForm state={state} dispatch={dispatch} />
+      <Events state={state} dispatch={dispatch} />
+      
+    </div>
+  );
+}
+
+
