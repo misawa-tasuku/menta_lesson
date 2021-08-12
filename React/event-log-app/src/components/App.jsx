e12beffef8d4533de9e17c92e1687049bf7cfd2c
misawa-tasuku <plusnine339@gmail.com>
2021-08-12 16:33:29 +0900

    setup-text-provider

--- React/event-log-app/src/components/App.jsx
+++ React/event-log-app/src/components/App.jsx
@@ -2,17 +2,23 @@ import React, {useReducer} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reducer from '../reducers'
 import Events from './Events'
+import AppContext from '../contexts/AppContext'
 import EventForm from './EventForm'
 
+console.log({AppContext})
+
 export const App = () => {
+  
   const [state, dispatch] = useReducer(reducer, [])
   
   return (
+    <AppContext.Provider value={'HELLO Provider'}>
     <div className="container-fluid">
       <EventForm state={state} dispatch={dispatch} />
       <Events state={state} dispatch={dispatch} />
       
     </div>
+    </AppContext.Provider>
   );
 }
 
