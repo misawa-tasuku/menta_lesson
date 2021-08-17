c4ef57b8dad7bcbed1ecbb605428626333d4d83e
misawa-tasuku <plusnine339@gmail.com>
2021-08-18 07:35:14 +0900

    create-operationLogs-incase-of-event-deletion

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,9 @@
 import React, {useContext} from 'react'
 
-import {DELETE_EVENT } from '../actions'
+import {DELETE_EVENT, ADD_OPERATION_LOG } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
+
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext)
@@ -9,7 +11,16 @@ const Event = ({ event }) => {
   const handleClickDeleteButton = () => {
     const result = window.confirm('イベントを削除しても良いですか?')
     // 定数定義によってバグを発見しやすい
-    if (result) dispatch({type: DELETE_EVENT, id})  
+    if (result) {
+      dispatch({type: DELETE_EVENT, id})  
+      
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: 'イベントを削除しました',
+        operatedAt: timeCurrentIso8601()
+      })
+    }
+
   }
     return(
       <tr>
