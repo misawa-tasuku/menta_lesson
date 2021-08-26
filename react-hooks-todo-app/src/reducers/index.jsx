524576a0340635c97f6fddd1d51571768f5cea37
misawa-tasuku <plusnine339@gmail.com>
2021-08-27 06:38:00 +0900

    create-reducer-for-todos

---
+++ react-hooks-todo-app/src/reducers/index.jsx
@@ -0,0 +1,17 @@
+const todos = (state = [], action) => {
+  switch(action.type){
+    case 'CREATE_TODO':
+      const todo = {title: action.title, body: action.body}
+      const length = state.length
+      const id = length === 0 ? 1 : state[length - 1].id + 1
+      return [...state, {id, ...todo}]
+    case 'DELETE_TODO':
+      return state
+    case 'DELETE_ALL_TODOS':
+      return []
+    default:
+      return state
+  }
+}
+
+export default todos
